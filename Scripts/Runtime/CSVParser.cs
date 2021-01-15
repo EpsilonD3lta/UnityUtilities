1897e213f3a1639233412623fc83589271d91940
EpsilonDelta <epsildel@gmail.com>
2021-01-15 19:51:20 +0100

    Improve CSVParser.cs

--- Scripts/Runtime/CSVParser.cs
+++ Scripts/Runtime/CSVParser.cs
@@ -1,32 +1,18 @@
-﻿using UnityEngine;
-
-public class CSVParser
+﻿public class CSVParser
 {
     private static char[] lineSeparators = new char[] { '\n' };
     private static char[] columnSeparators = new char[] { ',' };
 
-    public static string[,] GetCSVTable(string csvText)
+    public static string[][] GetCSVTable(string csvText)
     {
         // Get array of lines (get all rows)
         string[] lines = csvText.Split(lineSeparators, System.StringSplitOptions.RemoveEmptyEntries);
 
-        // Find maximum number of columns
-        int columns = 0;
-        for (int i = 0; i < lines.Length; i++)
-        {
-            string[] row = lines[i].Split(columnSeparators);
-            columns = Mathf.Max(columns, row.Length);
-        }
-
-        // Create 2D table
-        string[,] table = new string[lines.Length, columns];
+        // Create 2D jagged table
+        string[][] table = new string[lines.Length][];
         for (int y = 0; y < lines.Length; y++)
         {
-            string[] row = lines[y].Split(columnSeparators);
-            for (int x = 0; x < row.Length; x++)
-            {
-                table[y, x] = row[x];
-            }
+            table[y] = lines[y].Split(columnSeparators);
         }
 
         return table;
