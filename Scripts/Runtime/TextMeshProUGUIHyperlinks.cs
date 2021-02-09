6b493a66e8b10c3b13a64e576d301d30224e6814
EpsilonDelta <epsildel@gmail.com>
2021-02-09 16:53:46 +0100

    Add TextMeshPro Hyperlinks
    * Add forgoten using to EditorExtensions.cs

---
+++ Scripts/Runtime/TextMeshProUGUIHyperlinks.cs
@@ -0,0 +1,29 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+// Inspired by https://www.feelouttheform.net/unity3d-links-textmeshpro/
+[RequireComponent(typeof(TextMeshProUGUI))]
+public class TextMeshProUGUIHyperlinks : MonoBehaviour, IPointerClickHandler
+{
+    TextMeshProUGUI textMeshPro;
+
+    void Awake()
+    {
+        textMeshPro = GetComponent<TextMeshProUGUI>();
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        var cam = Camera.main;
+        if (textMeshPro.canvas.renderMode == RenderMode.ScreenSpaceOverlay) cam = null;
+        else if (textMeshPro.canvas.worldCamera != null) cam = textMeshPro.canvas.worldCamera;
+
+        int linkIndex = TMP_TextUtilities.FindIntersectingLink(textMeshPro, Input.mousePosition, cam);
+        if (linkIndex != -1) // Was a link clicked?
+        {
+            TMP_LinkInfo linkInfo = textMeshPro.textInfo.linkInfo[linkIndex];
+            Application.OpenURL(linkInfo.GetLinkID());
+        }
+    }
+}
