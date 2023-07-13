7ed658e750dd2b158d2e514c13f7e6f1bf0801c2
EpsilonDelta <epsildel@gmail.com>
2023-07-13 15:56:19 +0200

    Improve TouchInput

--- Scripts/Runtime/TouchInput.cs
+++ Scripts/Runtime/TouchInput.cs
@@ -1,18 +1,39 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Script execution order should be set to -1 or less
+/// </summary>
 public class TouchInput : MonoBehaviour
 {
-    public class TouchData
+    public Finger this[int i]
     {
-        public Vector2 initialContactPoint;
-        public Touch touch;
+        get { return fingers[i]; }
     }
 
-    public event Action<TouchData> touchStarted, touchStayed, touchMoved, touchEnded;
+    public class Finger
+    {
+        public event Action<Finger> touchStarted, touchStayed, touchMoved, touchEnded;
+        public event Action<Finger> dragStarted, dragged, dragEnded;
+        public Vector2 pressPosition;
+        public Touch touch;
 
-    private Dictionary<int, TouchData> activeTouches = new Dictionary<int, TouchData>();
+        public void InvokeTouchStarted() => touchStarted?.Invoke(this);
+        public void InvokeTouchStayed() => touchStayed?.Invoke(this);
+        public void InvokeTouchMoved() => touchMoved?.Invoke(this);
+        public void InvokeTouchEnded() => touchEnded?.Invoke(this);
+        public void InvokeDragStarted() => dragStarted?.Invoke(this);
+        public void InvokeDragged() => dragged?.Invoke(this);
+        public void InvokeDragEnded() => dragEnded?.Invoke(this);
+    }
+
+    public event Action<Finger> touchStarted, touchStayed, touchMoved, touchEnded;
+    public event Action<Finger> dragStarted, dragged, dragEnded;
+
+    private Dictionary<int, Finger> fingers = new Dictionary<int, Finger>();
+    private Dictionary<int, Finger> drags = new Dictionary<int, Finger>();
 
     private void Update()
     {
@@ -22,27 +43,82 @@ public class TouchInput : MonoBehaviour
             switch (touch.phase)
             {
                 case TouchPhase.Began:
-                    activeTouches[touch.fingerId] = new TouchData()
+                    var finger = new Finger()
                     {
-                        initialContactPoint = touch.position,
+                        pressPosition = touch.position,
                         touch = touch
                     };
-                    touchStarted?.Invoke(activeTouches[touch.fingerId]);
+                    fingers[touch.fingerId] = finger;
+                    touchStarted?.Invoke(finger);
+                    finger.InvokeTouchStarted();
                     break;
                 case TouchPhase.Stationary:
-                    activeTouches[touch.fingerId].touch = touch;
-                    touchStayed?.Invoke(activeTouches[touch.fingerId]);
+                    finger = fingers[touch.fingerId];
+                    finger.touch = touch;
+                    touchStayed?.Invoke(finger);
+                    finger.InvokeTouchStayed();
                     break;
                 case TouchPhase.Moved:
-                    activeTouches[touch.fingerId].touch = touch;
-                    touchMoved?.Invoke(activeTouches[touch.fingerId]);
+                    finger = fingers[touch.fingerId];
+                    finger.touch = touch;
+                    touchMoved?.Invoke(fingers[touch.fingerId]);
+                    finger.InvokeTouchMoved();
+                    HandleDrags(fingers[touch.fingerId]);
                     break;
                 case TouchPhase.Ended:
                 case TouchPhase.Canceled:
-                    touchEnded?.Invoke(activeTouches[touch.fingerId]);
-                    activeTouches.Remove(touch.fingerId);
+                    finger = fingers[touch.fingerId];
+                    finger.touch = touch;
+                    touchEnded?.Invoke(finger);
+                    if (drags.ContainsKey(touch.fingerId))
+                    {
+                        dragEnded?.Invoke(drags[touch.fingerId]);
+                        finger.InvokeDragEnded();
+                    }
+                    StartCoroutine(RemoveAtTheEndOfFrame(finger));
                     break;
             }
+        }
+    }
+
+    private void OnDisable()
+    {
+        foreach (var kvp in fingers)
+        {
+            touchEnded?.Invoke(kvp.Value);
+            kvp.Value.InvokeTouchEnded();
+            var touch = kvp.Value.touch;
+            if (drags.ContainsKey(touch.fingerId))
+            {
+                dragEnded?.Invoke(drags[touch.fingerId]);
+                kvp.Value.InvokeDragEnded();
+            }
+        }
+    }
+
+    // Keep fingers until the end of frame, so that we can work with them in all scripts' Update functions etc.
+    private WaitForEndOfFrame wait = new();
+    private IEnumerator RemoveAtTheEndOfFrame(Finger finger)
+    {
+        yield return wait;
+        var touch = finger.touch;
+        if (drags.ContainsKey(touch.fingerId))
+            drags.Remove(touch.fingerId);
+        fingers.Remove(touch.fingerId);
+    }
+
+    private void HandleDrags(Finger finger)
+    {
+        if (drags.ContainsKey(finger.touch.fingerId))
+        {
+            dragged?.Invoke(finger);
+            finger.InvokeDragged();
+        }
+        else if ((finger.touch.position - finger.pressPosition).sqrMagnitude > 100)
+        {
+            drags[finger.touch.fingerId] = finger;
+            dragStarted?.Invoke(finger);
+            finger.InvokeDragStarted();
         }
     }
 }
