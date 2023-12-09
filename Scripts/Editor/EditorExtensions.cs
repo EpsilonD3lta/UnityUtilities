207d8a4afd2647bc19cbd971c3c728495e3328d0
EpsilonDelta <epsildel@gmail.com>
2023-12-09 18:33:37 +0100

    Improve FindAssetUsages
    * Improve EditorExtensions

--- Scripts/Editor/EditorExtensions.cs
+++ Scripts/Editor/EditorExtensions.cs
@@ -3,17 +3,12 @@ using UnityEngine;
 
 public static class EditorExtensions
 {
-    // Unity UI Extensions License (BSD3) Copyright (c) 2019
-    // https://bitbucket.org/UnityUIExtensions/unity-ui-extensions/src/release/LICENSE.md
-    // License included in Licenses folder as BSD3_LICENSE_UnityUIExtensions.txt
-    // Modified
     [MenuItem("CONTEXT/RectTransform/Anchors to Corners")]
     static void AnchorsToCorners(MenuCommand command)
     {
         if (Selection.transforms == null || Selection.transforms.Length == 0)
-        {
             return;
-        }
+
         Undo.IncrementCurrentGroup();
         Undo.SetCurrentGroupName("AnchorsToCorners");
         var undoGroup = Undo.GetCurrentGroup();
@@ -21,16 +16,15 @@ public static class EditorExtensions
         foreach (Transform transform in Selection.transforms)
         {
             RectTransform t = transform as RectTransform;
-            Undo.RecordObject(t, "AnchorsToCorners");
             RectTransform pt = Selection.activeTransform.parent as RectTransform;
-
             if (t == null || pt == null) return;
+
+            Undo.RecordObject(t, "AnchorsToCorners");
 
             Vector2 newAnchorsMin = new Vector2(t.anchorMin.x + t.offsetMin.x / pt.rect.width,
                                                 t.anchorMin.y + t.offsetMin.y / pt.rect.height);
             Vector2 newAnchorsMax = new Vector2(t.anchorMax.x + t.offsetMax.x / pt.rect.width,
                                                 t.anchorMax.y + t.offsetMax.y / pt.rect.height);
-
             t.anchorMin = newAnchorsMin;
             t.anchorMax = newAnchorsMax;
             t.offsetMin = t.offsetMax = new Vector2(0, 0);
@@ -38,17 +32,12 @@ public static class EditorExtensions
         Undo.CollapseUndoOperations(undoGroup);
     }
 
-    // Unity UI Extensions License (BSD3) Copyright (c) 2019
-    // https://bitbucket.org/UnityUIExtensions/unity-ui-extensions/src/release/LICENSE.md
-    // License included in Licenses folder BSD3_LICENSE_UnityUIExtensions.txt
-    // Modified
     [MenuItem("CONTEXT/RectTransform/Corners to Anchors")]
     static void CornersToAnchors(MenuCommand command)
     {
         if (Selection.transforms == null || Selection.transforms.Length == 0)
-        {
             return;
-        }
+
         Undo.IncrementCurrentGroup();
         Undo.SetCurrentGroupName("CornersToAnchors");
         var undoGroup = Undo.GetCurrentGroup();
@@ -56,10 +45,9 @@ public static class EditorExtensions
         foreach (Transform transform in Selection.transforms)
         {
             RectTransform t = transform as RectTransform;
+            if (t == null) continue;
+
             Undo.RecordObject(t, "CornersToAnchors");
-
-            if (t == null) return;
-
             t.offsetMin = t.offsetMax = new Vector2(0, 0);
         }
         Undo.CollapseUndoOperations(undoGroup);
--- Scripts/Editor/FindAssetUsages.cs
+++ Scripts/Editor/FindAssetUsages.cs
@@ -2,14 +2,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
 using UnityEditor;
-using UnityEditor.PackageManager.UI;
 using UnityEngine;
 
-// Adapted from script by Michal Ferko:
-// https://github.com/michalferko
-// https://www.linkedin.com/in/michalferko/
-// This script is distributed under MIT License included in Licenses folder as MIT_LICENSE_MichalFerko
 public class FindAssetUsages : EditorWindow
 {
     // Look for usages only in these assets (and their .meta files)
@@ -29,48 +26,50 @@ public class FindAssetUsages : EditorWindow
     };
 
     private List<string> assetPaths = new List<string>();
-    private string selectedAssetGUID = null;
-    private bool canceled = false;
+    private string selectedAssetGuid;
+    private string selectedAssetPath;
+    private string selectedAssetName;
     private Vector2 scroll = Vector2.zero;
+    private int progressId;
+    private float progress;
+    private bool isSearching;
+    private CancellationTokenSource tokenSource;
 
     [MenuItem("Assets/Find Asset Usage _#F12")]
     public static void CreateWindow()
     {
-        var window = GetWindow<FindAssetUsages>();
+        var window = CreateWindow<FindAssetUsages>();
         window.Show();
-        window.canceled = false;
-
-        if (EditorUtility.DisplayCancelableProgressBar("Searching...", "Searching for asset references", 0))
-        {
-            window.canceled = true;
-            EditorUtility.ClearProgressBar();
-            return;
-        }
 
         string[] assetGuids = Selection.assetGUIDs;
         if (assetGuids.Length == 0)
         {
             Debug.Log("Cannot find asset usages when no assets are selected.");
-            EditorUtility.ClearProgressBar();
             return;
         }
-        if (assetGuids.Length > 1)
-        {
-            Debug.Log("Finding asset usages no multiple assets support - only the first asset references will be found");
-        }
 
-        window.selectedAssetGUID = assetGuids[0];
-        window.assetPaths = FindAssetUsage(window.selectedAssetGUID, ref window.canceled);
+        window.selectedAssetGuid = assetGuids[0];
+        window.selectedAssetPath = AssetDatabase.GUIDToAssetPath(window.selectedAssetGuid).Replace("/", "\\");
+        window.selectedAssetName = Path.GetFileName(window.selectedAssetPath);
+
+        _ = window.FindAssetUsage(window.selectedAssetGuid);
+    }
+
+    private void OnEnable()
+    {
+        Progress.added += OnSearchStarted;
+        Progress.updated += OnSearchUpdated;
+        Progress.removed += OnSearchEnded;
     }
 
     private void OnGUI()
     {
-        if (selectedAssetGUID == null)
+        if (selectedAssetGuid == null)
         {
             EditorGUILayout.LabelField("Right click on an Asset and select 'Find Asset Usages'");
             return;
         }
-        string searchedAssetFilename = AssetDatabase.GUIDToAssetPath(selectedAssetGUID);
+        string searchedAssetFilename = AssetDatabase.GUIDToAssetPath(selectedAssetGuid);
         if (searchedAssetFilename == null)
         {
             EditorGUILayout.LabelField("Right click on an Asset and select 'Find Asset Usages'");
@@ -90,11 +89,18 @@ public class FindAssetUsages : EditorWindow
         GUIContent searchContent = EditorGUIUtility.IconContent("Search Icon");
         if (GUILayout.Button(searchContent, GUILayout.MaxWidth(40), GUILayout.MaxHeight(18)))
         {
-            if (!string.IsNullOrEmpty(selectedAssetGUID))
-                assetPaths = FindAssetUsage(selectedAssetGUID, ref canceled);
+            if (!string.IsNullOrEmpty(selectedAssetGuid))
+                _ = FindAssetUsage(selectedAssetGuid);
         }
         GUILayout.EndHorizontal();
         EditorGUILayout.Space();
+        if (isSearching)
+        {
+            EditorGUI.ProgressBar(GUILayoutUtility.GetRect(position.width, 20),
+                progress, $"Searching {Mathf.RoundToInt((progress * 100))}%");
+        }
+
+
         GUILayout.Label("Found Asset References", EditorStyles.boldLabel);
         EditorGUILayout.Space();
 
@@ -115,7 +121,109 @@ public class FindAssetUsages : EditorWindow
         EditorGUILayout.EndScrollView();
     }
 
-    public static List<string> FindAssetUsage(string assetGuid, ref bool canceled)
+    private void OnDisable()
+    {
+        Progress.added -= OnSearchStarted;
+        Progress.updated -= OnSearchUpdated;
+        Progress.removed -= OnSearchEnded;
+        CancelSearch();
+    }
+
+
+    private async Task FindAssetUsage(string assetGuid)
+    {
+        CancelSearch();
+        tokenSource = new CancellationTokenSource();
+        progressId = Progress.Start($"Find Asset Usages {selectedAssetName}");
+        Progress.RegisterCancelCallback(progressId, CancelSearch);
+        assetPaths = await FindAssetUsageAsync(assetGuid, tokenSource.Token, progressId);
+        Repaint();
+        tokenSource = null;
+    }
+
+    public static async Task<List<string>> FindAssetUsageAsync(string assetGuid, CancellationToken token, int? progressId = null,
+        bool showProgressBar = false)
+    {
+        var usedByAssetsPaths = new List<string>(); // Empty old results
+        float progress = 0;
+        string projectPath = Application.dataPath.Substring(0, Application.dataPath.Length - 7).Replace("/", "\\");
+        var otherFilesPaths = Directory.EnumerateFiles(projectPath + "\\Assets", "*", SearchOption.AllDirectories).ToList();
+        otherFilesPaths.AddRange(Directory.EnumerateFiles(projectPath + "\\ProjectSettings", "*", SearchOption.AllDirectories).ToList());
+        List<string> extensionsWithMeta = new List<string>(extensions);
+        extensionsWithMeta.AddRange(extensions.Select(x => x + ".meta"));
+        otherFilesPaths = otherFilesPaths.Where(x => Regex.IsMatch(x, $"\\.({string.Join("|", extensionsWithMeta)})$")).ToList();
+
+        string assetPath = AssetDatabase.GUIDToAssetPath(assetGuid).Replace("/", "\\");
+        var assetName = Path.GetFileName(assetPath);
+        string assetFilePath = projectPath + "\\" + assetPath;
+        string assetMetaFilePath = assetFilePath + ".meta";
+        Regex regex = new Regex(assetGuid, RegexOptions.Compiled);
+
+        if (progressId == null)
+            progressId = Progress.Start($"Find Asset Usages {assetName}");
+        new ItemProgress(progressId.Value, showProgressBar);
+        await Task.Run(() => Find());
+        return usedByAssetsPaths;
+
+        void Find()
+        {
+            int total = otherFilesPaths.Count;
+            int current = 0;
+            foreach (string otherFilePath in otherFilesPaths)
+            {
+                if (token.IsCancellationRequested) break;
+                if (!File.Exists(otherFilePath))
+                {
+                    Debug.LogWarning($"File does not exist, path too long? Path: {otherFilePath}");
+                    continue;
+                }
+                var otherFileName = Path.GetFileName(otherFilePath);
+                progress = current / (float)total;
+                Progress.Report(progressId.Value, current, total, otherFileName);
+                current++;
+
+                if (regex.IsMatch(File.ReadAllText(otherFilePath)))
+                {
+                    if (assetFilePath == otherFilePath || assetMetaFilePath == otherFilePath) continue;
+                    string otherFileAssetPath = otherFilePath.Replace(projectPath + "\\", "").Replace("\\", "/");
+                    usedByAssetsPaths.Add(otherFileAssetPath);   // Not referencing self, add ref
+                }
+            }
+            Progress.Remove(progressId.Value);
+        }
+    }
+
+    private bool CancelSearch()
+    {
+        if (tokenSource != null) tokenSource.Cancel();
+        return true;
+    }
+
+    private void OnSearchStarted(Progress.Item[] items)
+    {
+        var item = items.FirstOrDefault(x => x.id == progressId);
+        if (item == null) return;
+        isSearching = true;
+        Repaint();
+    }
+
+    private void OnSearchUpdated(Progress.Item[] items)
+    {
+        var item = items.FirstOrDefault(x => x.id == progressId);
+        if (item == null) return;
+        progress = item.progress;
+        Repaint();
+    }
+
+    private void OnSearchEnded(Progress.Item[] items)
+    {
+        var item = items.FirstOrDefault(x => x.id == progressId);
+        if (item == null) return;
+        isSearching = false;
+        Repaint();
+    }
+
+    public static List<string> FindAssetUsageSync(string assetGuid)
     {
         var usedByAssetsPaths = new List<string>(); // Empty old results
 
@@ -130,6 +238,7 @@ public class FindAssetUsages : EditorWindow
         int current = 0;
 
         string assetPath = AssetDatabase.GUIDToAssetPath(assetGuid).Replace("/", "\\");
+        var assetName = Path.GetFileName(assetPath);
         string assetFilePath = projectPath + "\\" + assetPath;
         string assetMetaFilePath = assetFilePath + ".meta";
         Regex regex = new Regex(assetGuid, RegexOptions.Compiled);
@@ -141,13 +250,13 @@ public class FindAssetUsages : EditorWindow
                 Debug.LogWarning($"File does not exist, path too long? Path: {otherFilePath}");
                 continue;
             }
-            if (EditorUtility.DisplayCancelableProgressBar("Searching...", "Searching for asset references", current / (float)total))
+            var otherFileName = Path.GetFileName(otherFilePath);
+            float progress = current / (float)total;
+            if (EditorUtility.DisplayCancelableProgressBar("Searching...", "Searching for asset references", progress))
             {
-                canceled = true;
                 EditorUtility.ClearProgressBar();
                 return usedByAssetsPaths;
             }
-
             current++;
 
             if (regex.IsMatch(File.ReadAllText(otherFilePath)))
@@ -159,5 +268,51 @@ public class FindAssetUsages : EditorWindow
         }
         EditorUtility.ClearProgressBar();
         return usedByAssetsPaths;
+    }
+
+    public class ItemProgress
+    {
+        public int progressId;
+        public float progress;
+        public bool showProgressBar;
+
+        public ItemProgress(int progressId, bool showProgressBar = false)
+        {
+            this.progressId = progressId;
+            this.showProgressBar = showProgressBar;
+            Progress.added += OnSearchStarted;
+            Progress.updated += OnSearchUpdated;
+            Progress.removed += OnSearchEnded;
+        }
+
+        private void OnSearchStarted(Progress.Item[] items)
+        {
+            var item = items.FirstOrDefault(x => x.id == progressId);
+            if (item == null) return;
+        }
+
+        private void OnSearchUpdated(Progress.Item[] items)
+        {
+            var item = items.FirstOrDefault(x => x.id == progressId);
+            if (item == null) return;
+            progress = item.progress;
+            if (showProgressBar)
+            {
+                if (EditorUtility.DisplayCancelableProgressBar("Searching...", "Searching for asset references", progress))
+                {
+                    EditorUtility.ClearProgressBar();
+                }
+            }
+        }
+
+        private void OnSearchEnded(Progress.Item[] items)
+        {
+            var item = items.FirstOrDefault(x => x.id == progressId);
+            if (item == null) return;
+            EditorUtility.ClearProgressBar();
+            Progress.added -= OnSearchStarted;
+            Progress.updated -= OnSearchUpdated;
+            Progress.removed -= OnSearchEnded;
+        }
     }
 }
