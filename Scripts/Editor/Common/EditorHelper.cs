c9cd5a0c50422fb2b1cc65130794fa83b9773e80
EpsilonDelta <epsildel@gmail.com>
2024-01-12 14:43:31 +0100

    Add MyShortcuts.cs

--- Scripts/Editor/Common/EditorHelper.cs
+++ Scripts/Editor/Common/EditorHelper.cs
@@ -95,37 +95,6 @@ public class EditorHelper
         }
     }
 
-    [Shortcut("Mute Game View Audio", KeyCode.M)]
-    public static void MuteGameViewAudio()
-    {
-        EditorUtility.audioMasterMute = !EditorUtility.audioMasterMute;
-    }
-
-    [Shortcut("Lock Inspector", KeyCode.C, ShortcutModifiers.Alt)]
-    public static void ToggleInspectorLock()
-    {
-        ActiveEditorTracker.sharedTracker.isLocked = !ActiveEditorTracker.sharedTracker.isLocked;
-        ActiveEditorTracker.sharedTracker.ForceRebuild();
-    }
-
-    [Shortcut("Lock Project Tab", KeyCode.V, ShortcutModifiers.Alt)]
-    public static void ToggleProjectTabLock()
-    {
-        var unityEditorAssembly = Assembly.GetAssembly(typeof(Editor));
-        var projectBrowserType = unityEditorAssembly.GetType("UnityEditor.ProjectBrowser");
-        var projectBrowsers = Resources.FindObjectsOfTypeAll(projectBrowserType);
-        var isLockedProperty = projectBrowserType.GetProperty("isLocked", BindingFlags.Instance | BindingFlags.NonPublic);
-
-        foreach (var p in projectBrowsers)
-        {
-            var isLockedOldValue = (bool)isLockedProperty.GetValue(p);
-            isLockedProperty.SetValue(p, !isLockedOldValue);
-
-            EditorWindow pw = (EditorWindow)p;
-            pw.Repaint();
-        }
-    }
-
     public static void SelectWithoutFocus(params Object[] objects)
     {
         var unityEditorAssembly = Assembly.GetAssembly(typeof(Editor));
---
+++ Scripts/Editor/MyShortcuts.cs
@@ -0,0 +1,44 @@
+using UnityEditor.ShortcutManagement;
+using UnityEditor;
+using UnityEngine;
+using System.Reflection;
+
+public class MyShortcuts
+{
+    [Shortcut("Delete Alternative", KeyCode.X)]
+    public static void DeleteAlternative()
+    {
+        EditorApplication.ExecuteMenuItem("Edit/Delete");
+    }
+
+    [Shortcut("Mute Game View Audio", KeyCode.M)]
+    public static void MuteGameViewAudio()
+    {
+        EditorUtility.audioMasterMute = !EditorUtility.audioMasterMute;
+    }
+
+    [Shortcut("Lock Inspector", KeyCode.C, ShortcutModifiers.Alt)]
+    public static void ToggleInspectorLock()
+    {
+        ActiveEditorTracker.sharedTracker.isLocked = !ActiveEditorTracker.sharedTracker.isLocked;
+        ActiveEditorTracker.sharedTracker.ForceRebuild();
+    }
+
+    [Shortcut("Lock Project Tab", KeyCode.V, ShortcutModifiers.Alt)]
+    public static void ToggleProjectTabLock()
+    {
+        var unityEditorAssembly = Assembly.GetAssembly(typeof(Editor));
+        var projectBrowserType = unityEditorAssembly.GetType("UnityEditor.ProjectBrowser");
+        var projectBrowsers = Resources.FindObjectsOfTypeAll(projectBrowserType);
+        var isLockedProperty = projectBrowserType.GetProperty("isLocked", BindingFlags.Instance | BindingFlags.NonPublic);
+
+        foreach (var p in projectBrowsers)
+        {
+            var isLockedOldValue = (bool)isLockedProperty.GetValue(p);
+            isLockedProperty.SetValue(p, !isLockedOldValue);
+
+            EditorWindow pw = (EditorWindow)p;
+            pw.Repaint();
+        }
+    }
+}
