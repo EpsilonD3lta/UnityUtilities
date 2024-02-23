9dfbac4d3c1e51ea4c30181dcf71b1580aed32e7
EpsilonDelta <epsildel@gmail.com>
2024-02-23 12:26:53 +0100

    Improve ComponentUtilities, implement property copying

--- Scripts/Editor/ComponentUtilities.cs
+++ Scripts/Editor/ComponentUtilities.cs
@@ -3,8 +3,12 @@ using UnityEngine;
 
 public class ComponentUtilities
 {
-    private static SerializedObject savedObject;
+    private static SerializedObject savedSerializedObject;
     private static Object savedTargetObject;
+
+    // Property copying
+    private static SerializedObject savedSerializedObjectForProperty;
+    private static string savedPropertyPath;
 
     [InitializeOnLoadMethod]
     static void Initialize()
@@ -14,11 +18,12 @@ public class ComponentUtilities
 
     public static void CopyOnContextClickWithModifiers(GenericMenu menu, SerializedProperty property)
     {
+        AddCopyPastePropertyOption(menu, property);
         // Ctrl == copy, shift == paste, alt == copy, ctrl + alt == cut, ctrl + shift == adaptive paste
         var modifiers = Event.current.modifiers;
         if (modifiers == EventModifiers.Control || modifiers == (EventModifiers.Control | EventModifiers.Alt))
         {
-            savedObject = new SerializedObject(property.serializedObject.targetObject);
+            savedSerializedObject = new SerializedObject(property.serializedObject.targetObject);
             // savedObject.targetObject is null if we delete component in the meantime. Hence we create a copy
             savedTargetObject = Object.Instantiate(property.serializedObject.targetObject);
             if (savedTargetObject is Component component) // Can be a scriptableObject
@@ -29,7 +34,7 @@ public class ComponentUtilities
         // Paste properties with same names
         if (modifiers == (EventModifiers.Shift | EventModifiers.Control))
         {
-            if (savedObject == null)
+            if (savedSerializedObject == null)
             {
                 Debug.Log("Saved component is null");
                 return;
@@ -38,7 +43,7 @@ public class ComponentUtilities
         }
         else if (modifiers == EventModifiers.Shift)
         {
-            if (savedObject == null)
+            if (savedSerializedObject == null)
             {
                 Debug.Log("Saved component is null");
                 return;
@@ -87,33 +92,90 @@ public class ComponentUtilities
         }
     }
 
-    /// <summary>
-    /// Tries to find properties with the same name and type and copypastes values
-    /// </summary>
-    /// <param name="property"></param>
+    /// <summary> Tries to find properties with the same name and type and copypastes values </summary>
     private static void AdaptivePaste(SerializedProperty property)
     {
         Undo.RecordObject(property.serializedObject.targetObject, "Paste component values adaptively");
         SerializedObject destination = new SerializedObject(property.serializedObject.targetObject);
-        SerializedProperty savedObjectProperties = savedObject.GetIterator();
+        SerializedProperty sourceProperties = savedSerializedObject.GetIterator();
 
         // This if statement will skip script type so that we don't override the destination component's type
-        if (savedObjectProperties.NextVisible(true))
+        if (sourceProperties.NextVisible(true))
         {
-            while (savedObjectProperties.NextVisible(true)) // Iterate through all serializedProperties
+            while (sourceProperties.NextVisible(true)) // Iterate through all serializedProperties
             {
-                // Find corresponding property in destination component by name
-                SerializedProperty destinationProperty = destination.FindProperty(savedObjectProperties.name);
+                // Find corresponding property in destination component by path
+                SerializedProperty destinationProperty = destination.FindProperty(sourceProperties.propertyPath);
 
                 // Validate that the properties are present in both components, and that they're the same type
-                if (destinationProperty != null && destinationProperty.propertyType == savedObjectProperties.propertyType)
+                if (destinationProperty != null && destinationProperty.propertyType == sourceProperties.propertyType)
                 {
                     // Copy value from savedObject to destination component
-                    destination.CopyFromSerializedProperty(savedObjectProperties);
+                    destination.CopyFromSerializedProperty(sourceProperties);
                 }
             }
         }
         destination.ApplyModifiedProperties();
+        AssetDatabase.SaveAssets();
         Debug.Log("Component values adaptively pasted");
+    }
+
+    private static void AddCopyPastePropertyOption(GenericMenu menu, SerializedProperty property)
+    {
+        var propertyPath = property.propertyPath;
+        menu.AddItem(new GUIContent("Copy Property"), false,
+            () => CopyProperty(property.serializedObject, propertyPath));
+        if (savedSerializedObjectForProperty != null)
+        {
+            menu.AddItem(new GUIContent("Paste Property"), false,
+                () => PasteProperty(property.serializedObject, propertyPath));
+            menu.AddItem(new GUIContent("Paste Property Adaptively"), false,
+                () => PastePropertyAdaptively(property.serializedObject, propertyPath));
+        }
+        else
+            menu.AddDisabledItem(new GUIContent("Paste Property"), false);
+    }
+
+    private static void CopyProperty(SerializedObject serializedObject, string propertyPath)
+    {
+        savedPropertyPath = propertyPath;
+        savedSerializedObjectForProperty = new SerializedObject(serializedObject.targetObject);
+    }
+    private static void PasteProperty(SerializedObject destination, string propertyPath)
+    {
+        var sourceProperty = savedSerializedObjectForProperty.FindProperty(savedPropertyPath);
+
+        destination.CopyFromSerializedProperty(sourceProperty);
+        destination.ApplyModifiedProperties();
+        AssetDatabase.SaveAssets();
+    }
+
+    /// <summary>
+    /// Paste only child properties of a property, useful for SerializeReference properties pasting to not change type
+    /// </summary>
+    private static void PastePropertyAdaptively(SerializedObject destination, string propertyPath)
+    {
+        Undo.RecordObject(destination.targetObject, "Paste property values adaptively");
+        var sourceProperty = savedSerializedObjectForProperty.FindProperty(savedPropertyPath);
+        if (sourceProperty.propertyType != SerializedPropertyType.ManagedReference)
+        {
+            PasteProperty(destination, propertyPath);
+            return;
+        }
+
+        foreach (SerializedProperty prop in sourceProperty) // Iterate through all serializedProperties
+        {
+            // Find corresponding property in destination component by name
+            SerializedProperty destinationProperty = destination.FindProperty(propertyPath + "." + prop.name);
+
+            // Validate that the properties are present in both components, and that they're the same type
+            if (destinationProperty != null && destinationProperty.propertyType == prop.propertyType)
+            {
+                // Copy value from savedObject to destination component
+                destination.CopyFromSerializedProperty(prop);
+            }
+        }
+        destination.ApplyModifiedProperties();
+        AssetDatabase.SaveAssets();
     }
 }
